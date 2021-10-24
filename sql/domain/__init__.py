0c79c5c0cde02f3934b278ecea750602ac462519
BlaD200 <blad600@gmail.com>
2021-10-24 15:44:16 +0300

    Creates a migration scrip with alembic

--- sql/domain/__init__.py
+++ sql/domain/__init__.py
@@ -1,1 +1,9 @@
 #  Copyright (c) 2021 Vladyslav Synytsyn.
+
+from .DayScheduleEntity import DaySchedule, WeekDay
+from .FacultyEntity import Faculty
+from .GroupEntity import Group
+from .SubjectEntity import Subject, LessonType
+from .SubjectGroupEntity import SubjectGroup
+from .TeacherEntity import Teacher
+from .UserEntity import User
